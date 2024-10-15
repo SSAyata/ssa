61a09131b10e9476b58b91e004b186f7dc075478
SSAyata <soner.ayata@gmail.com>
2024-10-15 18:12:23 +0300

    Add files via upload

--- 05_LoopsWithStars/Program.cs
+++ 05_LoopsWithStars/Program.cs
@@ -193,21 +193,21 @@ namespace _05_LoopsWithStars
 
             #region Ters Piramit
 
-            //int n = 5;
-            //for (int i = n; i >= 1; i--)
-            //{
-            //    //Boşluk
-            //    for (int j = n - i; j > 0; j--)
-            //    {
-            //        Console.Write(" ");
-            //    }
-            //    //yıldız
-            //    for (int k = 1; k <= (2 * i) - 1; k++)
-            //    {
-            //        Console.Write("*");
-            //    }
-            //    Console.WriteLine();
-            //}
+            int n = 5;
+            for (int i = n; i >= 1; i--)
+            {
+                //Boşluk
+                for (int j = n - i; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                //yıldız
+                for (int k = 1; k <= (2 * i) - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
 
 
             #endregion
